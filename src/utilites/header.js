6b9a012664cde8acaf03eed065337c4dbfa748f8
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-12-10 15:24:35 +0200

    добавил открытие и закрытие модалок

--- src/utilites/header.js
+++ src/utilites/header.js
@@ -1,27 +1,65 @@
 import headerTmpl from '../templates/header.hbs';
 import searchModalTmpl from '../templates/modal_header-search.hbs';
-// import './modal-header-search';
+import registerModalTmpl from '../templates/modal_authorisation.hbs';
+import addBillModalTmpl from '../templates/modal-add-bill.hbs';
 
 const refs = {
   headerContainer: document.querySelector('.header-container'),
   searchModalContainer: document.querySelector('.search-modal'),
+  registerModalContainer: document.querySelector('.register-modal'),
+  addBillModalContainer: document.querySelector('.add-bill-modal'),
 };
 
 const headerMarkup = headerTmpl();
 const searchModalMarkup = searchModalTmpl();
+const registerModalMarkup = registerModalTmpl();
+const addBullModalMarkup = addBillModalTmpl();
 
 refs.headerContainer.innerHTML = headerMarkup;
 refs.searchModalContainer.innerHTML = searchModalMarkup;
+refs.registerModalContainer.innerHTML = registerModalMarkup;
+refs.addBillModalContainer.innerHTML = addBullModalMarkup;
 
-const openSearchModalBtn = document.querySelector('[data="modal-open"]');
+// это кнопки открывающие и закрывающие модальные окна
 
-const closeSearchModalBtn = document.querySelector('[data="modal-close"]');
+const btnRefs = {
+  openSearchModalBtn: document.querySelector('[data="modal-open"]'),
+  closeSearchModalBtn: document.querySelector('[data="modal-close"]'),
 
-const backdrop = document.querySelector('[data-modal]');
+  openRegisterModalBtn: document.querySelector(
+    '.js-registration-open-modal-btn',
+  ),
+  closeRegisterModalBtn: document.querySelector('.js-register-close-modal-btn'),
 
-openSearchModalBtn.addEventListener('click', toggleModal);
-closeSearchModalBtn.addEventListener('click', toggleModal);
+  openAddBillModalBtn: document.querySelector('[data-add-bill]'),
+  closeAddBillModalBtn: document.querySelector(
+    '[data-modal-add-bill-close-btn]',
+  ),
+};
+
+const backdropSearchModal = document.querySelector('[data-modal]');
+const backdropRegisterRegister = document.querySelector(
+  '[data-modal-register]',
+);
+const backdropAddBillhModal = document.querySelector('[data-modal-add-bill]');
+
+btnRefs.openSearchModalBtn.addEventListener('click', toggleModal);
+btnRefs.closeSearchModalBtn.addEventListener('click', toggleModal);
+
+btnRefs.openRegisterModalBtn.addEventListener('click', toggleModalRegister);
+btnRefs.closeRegisterModalBtn.addEventListener('click', toggleModalRegister);
+
+btnRefs.openAddBillModalBtn.addEventListener('click', toggleModalAddBill);
+btnRefs.closeAddBillModalBtn.addEventListener('click', toggleModalAddBill);
 
 function toggleModal() {
-  backdrop.classList.toggle('is-hidden');
+  backdropSearchModal.classList.toggle('is-hidden');
+}
+
+function toggleModalRegister() {
+  backdropRegisterRegister.classList.toggle('is-hidden');
+}
+
+function toggleModalAddBill() {
+  backdropAddBillhModal.classList.toggle('is-hidden');
 }
